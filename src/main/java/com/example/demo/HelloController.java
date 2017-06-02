4195f6138f2af10ec62c1f7c9229828b12e4bca6
baiqiufei <baiqiufei@migu.cn>
2017-06-02 10:49:34 +0800

    first pull

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/hello")
+	public String test()
+	{
+		return "Hello World!";
+	}
+}
